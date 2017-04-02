12a13cc481144e8bb46b754947f4c61b5310ae32
Denis Rosset <physics@denisrosset.com>
2017-04-02 19:06:51 +0800

    Update sumI.m

--- src/pack/sumI.m
+++ src/pack/sumI.m
@@ -1,7 +1,7 @@
 function res = sumI(vec)
 i = com.faacets.gluon.Interface.compile({
     'import com.faacets.gluon._'
-    'Interface[Seq[Double], Double]("sum", "seq") {'
+    'Interface[Seq[Double], Double]("sumI", "seq") {'
     'seq => seq.sum'
     '}'
                    });
